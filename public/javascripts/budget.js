7933c7cac140834b92bdacda9e5b26922875fdc7
Jason Buffin <jason@loverofmysoul.com>
2013-10-15 00:04:25 -0400

    budget data structure nailed down finally and observables figured out with data binds

--- public/javascripts/budget.js
+++ public/javascripts/budget.js
@@ -1,22 +1,22 @@
 var budgetVM;
 if (ko) {
-	var MonthlyAmountObservable = {
-		init : function(monthlyAmountJSON) {
-			this.original = monthlyAmountJSON;
-			this.year = ko.observable(monthlyAmountJSON.year);
-			this.month = ko.observable(monthlyAmountJSON.month);
-			this.amount = ko.observable(monthlyAmountJSON.amount);
-		}
-	};
 	var LineItemObservable = {
 			init : function(lineItemJSON) {
 				this.original = lineItemJSON;
 				this.category = ko.observable(lineItemJSON.category);
-				this.amounts = ko.observableArray([]);
-				for(index in lineItemJSON.amounts) {
-					var monthlyAmountObservable = Object.create(MonthlyAmountObservable);
-					monthlyAmountObservable.init(lineItemJSON.amounts[index]);
-					this.amounts().push(monthlyAmountObservable);
+				this.amount = ko.observable(lineItemJSON.amount);
+			}
+	};
+	var MonthObservable = {
+			init : function(monthJSON) {
+				this.original = monthJSON;
+				this.year = ko.observable(monthJSON.year);
+				this.month = ko.observable(monthJSON.month);
+				this.lineItems = ko.observableArray([]);
+				for(index in monthJSON.lineItems) {
+					var lineItemObservable = Object.create(LineItemObservable);
+					lineItemObservable.init(monthJSON.lineItems[index]);
+					this.lineItems.push(lineItemObservable);
 				}
 			}
 	};
@@ -25,58 +25,72 @@ if (ko) {
 			console.log('Setting the year to '+year);
 			this.year = ko.observable(year);
 			this.budget = ko.observableArray([]);
-			this.getUsedCategories();
 			this.getYearlyBudget();
+			this.lineItemCategories = ko.computed(function() {
+				var lineItemCategories = [];
+				budgetVM.budget().forEach(function(thatBudget) {
+					thatBudget.lineItems().forEach(function(thatLineItem) {
+						if(lineItemCategories.indexOf(thatLineItem.category()) === -1) {
+							lineItemCategories.push(thatLineItem.category());
+						}
+					});
+				});
+				return lineItemCategories;
+			});
 		}
 	};
-	budgetVM.getUsedCategories = function() {
-		console.log('getting the categories for '+this.year());
-		this.categories = ko.observableArray(['Income', 'Housing']);
+	budgetVM.computeLineItemAmount = function(monthBudget, categoryToMatch) {
+		console.log('computing line item amounts');
+		console.log(monthBudget.lineItems());
+		console.log(categoryToMatch);
+		var match = ko.utils.arrayFirst(monthBudget.lineItems(), function(item) {
+			console.log('does '+item.category()+' match '+categoryToMatch+'?');
+			return item.category() === categoryToMatch;
+		});
+		if(!match) {
+			console.log('no match');
+			return 'error';
+		} else {
+			return match.amount;
+		}
 	};
 	budgetVM.getYearlyBudget = function() {
 		console.log('getting the budget for '+this.year());
 		var fakeBudget = [{
-			_id : {
-				$oid : '5143lj34t1ib'
-			},
-			category : 'Income',
-			amounts : [{
-				year : 2013,
-				month : 1,
-				amount : 2900.00
+			month : 1,
+			year : 2013,
+			lineItems : [{
+				category : 'Income',
+				amount : 2901
 			}, {
-				year : 2013,
-				month : 2,
-				amount : 2900.00
-			}, {
-				year : 2013,
-				month : 3,
-				amount : 3000.00
+				category : 'Housing',
+				amount : 951
 			}]
 		}, {
-			_id : {
-				$oid : '5143lj3341ib'
-			},
-			category : 'Housing',
-			amounts : [{
-				year : 2013,
-				month : 1,
-				amount : 950.00
+			month : 2,
+			year : 2013,
+			lineItems : [{
+				category : 'Income',
+				amount : 2902
 			}, {
-				year : 2013,
-				month : 2,
-				amount : 950.00
+				category : 'Housing',
+				amount : 952
+			}]
+		}, {
+			month : 3,
+			year : 2013,
+			lineItems : [{
+				category : 'Income',
+				amount : 2903
 			}, {
-				year : 2013,
-				month : 3,
-				amount : 950.00
+				category : 'Housing',
+				amount : 953
 			}]
 		}];
 		for(index in fakeBudget) {
-			console.log(fakeBudget[index]);
-			var lineItemObservable = Object.create(LineItemObservable);
-			lineItemObservable.init(fakeBudget[index]);
-			this.budget().push(lineItemObservable);
+			var monthObservable = Object.create(MonthObservable);
+			monthObservable.init(fakeBudget[index]);
+			this.budget().push(monthObservable);
 		}
 	};
 	budgetVM.printFullMonth = function(month) {
